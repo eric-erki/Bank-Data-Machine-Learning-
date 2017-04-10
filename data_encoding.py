c36c66699d16b8397d89f577b18e3e095c002ffa
Hugo Fragata <hugofragata@ua.pt>
2017-04-10 14:21:25 +0100

    Logistic Regression Working. Beats most common target class baseline.

--- data_encoding.py
+++ data_encoding.py
@@ -9,6 +9,11 @@ def one_hot(dtf, cols):
     df = df.drop(cols, axis=1)
     return df
 
+def label_vec(dtf):
+    df = deepcopy(dtf)
+    df.replace(to_replace=['no', 'yes'], value=[0,1], inplace=True)
+    return  df
+
 def main():
     BANK_VALIDATION = "bank-crossvalidation_new.csv"
 
@@ -16,14 +21,21 @@ def main():
                "duration",
                "campaign", "pdays", "previous", "poutcome", "y"]
     CATEGORICAL_COLUMNS = ["job", "marital", "education", "default", "housing", "loan", "contact", "day", "month",
-                           "poutcome", "y"]
+                           "poutcome"]
+    LABEL = "y"
 
     validation_set = pd.read_csv(BANK_VALIDATION, names=COLUMNS, skipinitialspace=True, skiprows=1)
+    validation_label_set = deepcopy(validation_set[LABEL])
+    del validation_set['y']
 
     # Vectorize the categorical columns
     df = one_hot(validation_set, cols=CATEGORICAL_COLUMNS)
 
+    #Vectorize the label column
+    df_label = label_vec(validation_label_set)
+
     print(df.head())
+    print(df_label.head())
 
 
 if __name__ == '__main__':
--- logistic_regression.py
+++ logistic_regression.py
@@ -1,24 +1,28 @@
 from copy import deepcopy
-import tflearn
+import sklearn.linear_model as lm
 import pandas as pd
+from data_encoding import one_hot, label_vec
 
 BANK_TRAINING = "bank-training_new.csv"
 BANK_VALIDATION = "bank-crossvalidation_new.csv"
 
 COLUMNS = ["age","job","marital","education","default","balance","housing","loan","contact","day","month","duration",
            "campaign","pdays","previous","poutcome","y"]
-
-training_set = pd.read_csv(BANK_TRAINING, names=COLUMNS, skipinitialspace=True, skiprows=1)
-training_label_set = deepcopy(training_set['y'])
-del training_set['y']
-
-validation_set = pd.read_csv(BANK_VALIDATION, names=COLUMNS, skipinitialspace=True, skiprows=1)
-validation_label_set = deepcopy(validation_set['y'])
-del validation_set['y']
-
 CATEGORICAL_COLUMNS = ["job","marital","education","default","housing","loan","contact","day","month","poutcome"]
 CONTINUOUS_COLUMNS = ["age","balance","duration","campaign","pdays","previous"]
 LABEL = "y"
+
+training_set = pd.read_csv(BANK_TRAINING, names=COLUMNS, skipinitialspace=True, skiprows=1)
+training_label_set = deepcopy(training_set[LABEL])
+del training_set[LABEL]
+training_set = one_hot(training_set, CATEGORICAL_COLUMNS)
+training_label_set = label_vec(training_label_set)
+
+validation_set = pd.read_csv(BANK_VALIDATION, names=COLUMNS, skipinitialspace=True, skiprows=1)
+validation_label_set = deepcopy(validation_set[LABEL])
+del validation_set[LABEL]
+validation_set = one_hot(validation_set, CATEGORICAL_COLUMNS)
+validation_label_set = label_vec(validation_label_set)
 
 print(validation_set.shape)
 print(validation_label_set.shape)
@@ -26,23 +30,9 @@ print(validation_set.head())
 print("\n\n")
 print(validation_label_set.head())
 
-#quit()
 
+model = lm.LogisticRegression()
 
-optimizer = tflearn.optimizers.SGD (learning_rate=0.01)
-shape = tflearn.layers.core.input_data (shape=training_set.shape)
-estimator = tflearn.layers.regression(shape, optimizer=optimizer, n_classes=2, learning_rate=0.01 )
+model.fit(training_set, training_label_set)
 
-model = tflearn.models.dnn.DNN(estimator, tensorboard_verbose=3)
-
-model.fit(training_set, training_label_set, n_epoch=10, batch_size=16, show_metric=True)
-
-predicts = model.predict(validation_set)
-
-total_right,total = 0,0
-for p,l in predicts, validation_label_set:
-    if p[1]==l :
-        total_right+=1
-    total +=1
-
-print("Accuracy: ", total_right/total)
+print(model.score(validation_set, validation_label_set))
